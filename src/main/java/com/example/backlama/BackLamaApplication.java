1cf0fa89d43e0286c8060724eb390995fa60a16d
gpennamoro <guilherme.moro@isohub.com.br>
2023-06-17 17:37:18 -0300

    project config

---
+++ src/main/java/com/example/backlama/BackLamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backlama/BackLamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backlama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
