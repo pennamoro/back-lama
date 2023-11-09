48dfe6eddede310dc8014409ee858384352932a3
gpennamoro <guilherme.moro@isohub.com.br>
2023-11-09 20:16:40 -0300

    removed recipes that already are on the lista pessoal

--- src/main/java/com/example/backlama/controllers/UsuarioController.java
+++ src/main/java/com/example/backlama/controllers/UsuarioController.java
@@ -361,6 +361,7 @@ public class UsuarioController {
                 }
             }
             if (!receitasRecomendadas.isEmpty()){
+                receitasRecomendadas.removeAll(receitasNaLista);
                 for(Receita receita : receitasRecomendadas){
                     receita.setUser(null);
                     receita.setFoto(null);
@@ -368,6 +369,7 @@ public class UsuarioController {
                 return new ResponseEntity<>(receitasRecomendadas, HttpStatus.OK);
             }
             receitasRecomendadas.addAll(receitaService.listarTodasReceitas());
+            receitasRecomendadas.removeAll(receitasNaLista);
             for(Receita receita : receitasRecomendadas){
                 receita.setUser(null);
                 receita.setFoto(null);
